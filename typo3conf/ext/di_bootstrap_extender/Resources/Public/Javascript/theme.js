27b2cc2805e5af918678b70997304a058b123861
Rene Sonntag <rene.sonntag@t-systems.com>
2016-09-05 21:17:34 +0200

    - initial upload

--- typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
+++ typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
@@ -1,17 +1,23 @@
 // user is at top position
 if(!$(window).scrollTop()) {
     $('header').addClass('header-top');
-    $('.navbar-brand-image img').addClass('header-logo-top');
+    $('.navbar-brand-image').addClass('navbar-brand-image-top');
+    $('.navbar-brand-image img').addClass('navbar-brand-image-logo-top');
+    $('.container > nav > ul > li > a').addClass('container-nav-a-top');
 }
 
 $(window).scroll(function() {
     // user scrolls to top position
     if ($(window).scrollTop() == 0) {
         $('header').addClass('header-top');
-        $('.navbar-brand-image img').addClass('header-logo-top');
+        $('.navbar-brand-image').addClass('navbar-brand-image-top');
+        $('.navbar-brand-image img').addClass('navbar-brand-image-logo-top');
+        $('.container > nav > ul > li > a').addClass('container-nav-a-top');
     // user leaves top position
     } else {
         $('header').removeClass('header-top');
-        $('.navbar-brand-image img').removeClass('header-logo-top');
+        $('.navbar-brand-image').removeClass('navbar-brand-image-top');
+        $('.navbar-brand-image img').removeClass('navbar-brand-image-logo-top');
+        $('.container > nav > ul > li > a').removeClass('container-nav-a-top');
     }
 });
