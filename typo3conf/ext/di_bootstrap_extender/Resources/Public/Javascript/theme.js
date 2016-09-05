f800c6305489bcab47122801f27966dd67aaa76c
Rene Sonntag <rene.sonntag@t-systems.com>
2016-09-05 12:37:58 +0200

    - initial upload

--- typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
+++ typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
@@ -1,14 +1,17 @@
 // user is at top position
 if(!$(window).scrollTop()) {
-    $('header').addClass('header_top');
+    $('header').addClass('header-top');
+    $('.navbar-brand-image img').addClass('header-logo-top');
 }
 
 $(window).scroll(function() {
     // user scrolls to top position
     if ($(window).scrollTop() == 0) {
-        $('header').addClass('header_top');
+        $('header').addClass('header-top');
+        $('.navbar-brand-image img').addClass('header-logo-top');
     // user leaves top position
     } else {
-        $('header').removeClass('header_top');
+        $('header').removeClass('header-top');
+        $('.navbar-brand-image img').removeClass('header-logo-top');
     }
 });
