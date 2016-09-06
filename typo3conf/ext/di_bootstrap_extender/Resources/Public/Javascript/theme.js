81d3bcd9ac89e3f617e331cef545a5783b1d5f96
Rene Sonntag <rene.sonntag@t-systems.com>
2016-09-06 20:26:00 +0200

    - initial upload

--- typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
+++ typo3conf/ext/di_bootstrap_extender/Resources/Public/Javascript/theme.js
@@ -1,3 +1,6 @@
+/*
+ * Fixed and minimize header
+ */
 // user is at top position
 if(!$(window).scrollTop()) {
     $('header').addClass('header-top');
@@ -21,3 +24,34 @@ $(window).scroll(function() {
         $('.container > nav > ul > li > a').removeClass('container-nav-a-top');
     }
 });
+
+
+/**
+ * Opens layer navigation
+ *
+ * @return void
+ */
+var activateNavLayer = function () {
+    // hide all other layers
+    $('.dropdown-menu').hide();
+
+    $this = $(this);
+
+    // open current layer
+    $this.siblings('.dropdown-menu').show();
+}
+
+/**
+ * Closes layer navigation
+ *
+ * @return void
+ */
+var deactivateNavLayer = function() {
+
+    if ($(".navbar-main:hover").length <= 0 && $(".navbar-main > li:hover").length <= 0) {
+        $('.dropdown-menu').hide();
+    };
+}
+
+$('.container-nav-a-top').mouseover(activateNavLayer);
+$('.container-nav-a-top').parent().mouseout(deactivateNavLayer);
