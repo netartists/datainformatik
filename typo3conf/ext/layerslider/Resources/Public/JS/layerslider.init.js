abd18cc7ab7cdf558c3008f500401d773516c976
Rene Sonntag <rene.sonntag@t-systems.com>
2016-10-01 20:04:30 +0200

    - initial upload

--- typo3conf/ext/layerslider/Resources/Public/JS/layerslider.init.js
+++
@@ -1,7 +0,0 @@
-if(typeof layersliderOptions != 'undefined'){
-    if($(layersliderOptions).length > 0){
-        $.each(layersliderOptions,function(k, v){
-            $('#layerslider-'+k).layerSlider(v);
-        })
-    }
-}
